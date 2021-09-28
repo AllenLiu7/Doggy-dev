488f5ba398008b77a1160c161fe39cf3ecaccb54
Allen Liu <allen6qi@gmail.com>
2021-09-28 16:40:40 +0800

    fixed server error handling bug

--- server/app.js
+++ server/app.js
@@ -41,7 +41,7 @@ app.get('/*', (req, res) => {
 });
 
 //Custom express error handler
-app.use((error, req, res) => {
+app.use((error, req, res, next) => {
   res.status(error.status || 500);
   res.json({
     status: error.status,
